0706a9e44b78524e7c6fbc23cb19fe78e47a1a12
LoganTann <logon313@hotmail.fr>
2021-03-25 20:15:10 +0100

    L'animation du logo dans le header est rendue indépendante
    Normalement le portage en single component est rendue possible
    + fix de tous les bugs connus

--- public/js/main.js
+++ public/js/main.js
@@ -1,43 +1,55 @@
-function mainPageScript() {
-    // positionNextStep est partagé entre les deux events : il est initialisé
-    // dans onResize plutôt que onScroll pour des raisons de performance
-    let GLOBAL = {
+function logo_accueil_script() {
+    // DECLARATIONS --------------------------------------------------------- //
+
+    /* positionNextStep est partagé entre les deux events : il est initialisé
+       dans onResize plutôt que onScroll pour des raisons de performance */
+    let SHARED = {
       timer_resize: null,
       positionNextStep: 300
     };
 
-    const logo_png = document.getElementById("logo_png");
-    const page_content = document.getElementById("page_content");
+    const logo_container = document.getElementById("logo_accueil");
+    const logo_png = document.querySelector("#logo_accueil>img");
 
-
+    // requestAnimationFrame : __évite__ de call + de fois que les FPS actuel
     const onScroll = function(e) {
-      // inutile d'exécuter plus de fois que le nombre de FPS actuel
       window.requestAnimationFrame(function(){
-        // BUG : F5 pas ontop -> bug
-        // BUG : resize après scroll
-        // console.log(GLOBAL.positionNextStep);
-        if (page_content.getBoundingClientRect().top < GLOBAL.positionNextStep) {
+        const container_rect = logo_container.getBoundingClientRect();
+        if ((container_rect.top + container_rect.height) < SHARED.positionNextStep) {
           logo_png.classList.add("animStep2");
         } else if (logo_png.classList.contains("animStep2")){
           logo_png.classList.remove("animStep2");
         }
       });
     }
+
+    /* timeout : __update après__ que l'utilisateur ait fini de resize (dans une
+       limite de temps raisonnable) */
     const onResize = function() {
-      clearTimeout(GLOBAL.timer_resize);
-      GLOBAL.timer_resize = setTimeout(function(){
-        const logo_rect = logo_png.getBoundingClientRect();
-        GLOBAL.positionNextStep = Math.floor(logo_rect.top + (logo_rect.height * 0.58));
+      clearTimeout(SHARED.timer_resize);
+      SHARED.timer_resize = setTimeout(function(){
+        // constantes 20% et 58% -> cf. CSS
+        const loto_flagPos = logo_png.getBoundingClientRect().height * 0.58;
+        const logo_top = logo_container.getBoundingClientRect().height * 0.2;
+        SHARED.positionNextStep = Math.floor(logo_top + loto_flagPos);
+
+        onScroll();
       }, 75);
-      // onScroll();
     };
-    window.addEventListener('resize', onResize);
-    window.addEventListener("scroll", onScroll);
-    onResize();
 
 
+    // CODE EFFECTIF -------------------------------------------------------- //
+    logo_png.addEventListener("load", function(){
+      window.addEventListener('resize', onResize);
+      window.addEventListener("scroll", onScroll);
+      onResize();
+    });
+}
+
+function mainPageScript() {
     const photosMembres = document.querySelectorAll('#membres .avatar img');
     const materialBoxInstance = M.Materialbox.init(photosMembres, {});
+    logo_accueil_script();
 }
 
 
