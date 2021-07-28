ee94aafc18b427308514e39cf76ac37e2ee40e4f
LoganTann <logon313@hotmail.fr>
2021-07-28 14:55:00 +0200

    Algo de catégories fini (semble fonctionnel) + support TypeScript

--- pages/articles/_.vue
+++ pages/articles/_.vue
@@ -7,43 +7,103 @@
       <div class="col m3 push-m1">
         <h4>Liste des articles</h4>
         <ul class="browser-default">
-          <li v-for="post of allArticles">
-            <article-link :to="post.path">{{post.title}}</article-link>
+          <li v-for="post of allArticles" :key="post.path">
+            <article-link :to="post.path">
+              {{ post.title }}
+            </article-link>
           </li>
         </ul>
-        {{generateBetterStructure(allArticles)}}
+        {{ allArticles }}
+        <hr>
+        {{ generateBetterStructure(allArticles) }}
       </div>
 
       <div class="col m7 push-m1">
         <h1>{{ article.title }}</h1>
-        <ArticleTOC :articleTOC="article.toc"/>
+        <ArticleTOC :article-t-o-c="article.toc" />
         <hr>
-        <nuxt-content :document="article"/>
+        <nuxt-content :document="article" />
         <hr>
         <h4>Navigation : </h4>
-        <article-prev-next :prev="prev" :next="next"/>
+        <article-prev-next :prev="prev" :next="next" />
       </div>
     </div>
   </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import Vue from 'vue'
+import { IContentDocument } from '@nuxt/content/types/content'
+
+interface articleMetadata {
+  'title': string, 'description': string,
+  'slug': string, 'path': string, 'dir': string,
+  'createdAt': string, 'updatedAt': string
+}
+interface articleTree {
+  [key: string]: {
+    sortBy: 'slug' | 'updatedAt',
+    content: {
+      type: 'article' | 'category'
+      content: articleMetadata
+    }[]
+  }
+}
+export default Vue.extend({
+  async asyncData ({ $content, params, error }) {
+    const path = `/${params.pathMatch || ''}`
+    const allowedPaths = { $in: [path, `${path}/index`, `${path}index`, `${path}/0`, `${path}0`] };
+
+    const allArticles = await $content({ deep: true })
+      .only(['title', 'description', 'slug', 'path', 'dir', 'path', 'createdAt', 'updatedAt'])
+      .fetch();
+
+    const [article] = (await $content({ deep: true })
+      .where({ path: allowedPaths })
+      .fetch()
+      ) as Array<IContentDocument>
+
+    if (!article) {
+      return error({ statusCode: 404, message: 'Article not found. Path : ' + path })
+    }
+
+    const [prev, next] = (await $content({ deep: true })
+      .only(['title', 'path'])
+      .where({ dir: article.dir })
+      .sortBy('path', 'asc')
+      .surround(article.path)
+      .fetch()
+      ) as Array<IContentDocument>
+
+    return {
+      allArticles,
+      article,
+      prev,
+      next
+    }
+  },
   methods: {
-    generateBetterStructure(allArticles) {
-      const result = {};
+    /**
+     * Given a list of articles, will return an object where key is the category and object->content contains the list
+     * of articles, correctly sorted (by date or by slug).
+     * @param allArticles
+     */
+    generateBetterStructure (allArticles: articleMetadata[]) {
+      const result: articleTree = {};
       // Will convert the path list into a tree
       for (const article of allArticles) {
         if (typeof result[article.dir] !== "object") {
-          result[article.dir] = { content: [], sortBy: "createdAt" };
+          result[article.dir] = { content: [], sortBy: "slug" };
         }
         const refDir = result[article.dir];
 
         switch (article.slug) {
-          case '0':
-            refDir.sortBy = "slug";
+          case 'index':
+            if (article.dir !== '/') {
+              refDir.sortBy = "updatedAt";
+            }
             // fall through
-          case 'index': {
+          case '0': {
             const parentDir = '/' + article.dir.split('/').slice(1, -1).join('/');
             if (article.dir !== '/' && result[parentDir]) {
               result[parentDir].content.push({ type: "category", content: article });
@@ -55,38 +115,15 @@ export default {
             break;
         }
       }
+      // will sort
+      for (const path in result) {
+        const category = result[path];
+        if (category.sortBy === 'updatedAt') {
+          category.content.sort((a, b) => b.content.updatedAt.localeCompare(a.content.updatedAt));
+        }
+      }
       return result;
     }
-  },
-  async asyncData ({ $content, params, error }) {
-    const path = `/${params.pathMatch || ''}`
-    const allowedPaths = { $in: [path, `${path}/index`, `${path}index`, `${path}/0`, `${path}0`] };
-
-    const allArticles = await $content({ deep: true })
-      .only(['title', 'description', 'slug', 'path', 'dir', 'path', 'createdAt', 'updatedAt'])
-      .fetch();
-
-    const [article] = await $content({ deep: true })
-      .where({ path: allowedPaths })
-      .fetch()
-
-    if (!article) {
-      return error({ statusCode: 404, message: 'Article not found. Path : ' + path })
-    }
-
-    const [prev, next] = await $content({ deep: true })
-      .only(['title', 'path'])
-      .where({ dir: article.dir })
-      .sortBy('path', 'asc')
-      .surround(article.path)
-      .fetch()
-
-    return {
-      allArticles,
-      article,
-      prev,
-      next
-    }
   }
-}
+});
 </script>
