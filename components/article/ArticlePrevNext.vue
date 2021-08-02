3cea7c81ddeceb9ab9091b7e199225089c6a925c
LoganTann <logon313@hotmail.fr>
2021-08-03 00:01:42 +0200

    bug fix Cannot read property path of null in article-prev-next component

--- components/article/ArticlePrevNext.vue
+++ components/article/ArticlePrevNext.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <article-link v-if="prev !== ''" :to="prev.path">
+    <article-link v-if="prev" :to="prev.path">
       {{ prev.title }} &lt;
     </article-link>
-    <article-link v-if="next !== ''" :to="next.path">
+    <article-link v-if="next" :to="next.path">
       &gt; {{ next.title }}
     </article-link>
   </div>
@@ -13,8 +13,8 @@
 import Vue from 'vue';
 export default Vue.extend({
   props: {
-    prev: { type: String, default: '' },
-    next: { type: String, default: '' }
+    prev: { type: Object, default: null },
+    next: { type: Object, default: null }
   }
 });
 </script>
