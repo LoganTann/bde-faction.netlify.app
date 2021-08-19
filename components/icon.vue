01e4d8c403499ff7a565c6cbe2860faa6f13552b
LoganTann <logon313@hotmail.fr>
2021-08-20 01:31:57 +0200

    contneu de cpu-paris fait, corrections à faire niveau design

---
+++ components/icon.vue
@@ -0,0 +1,17 @@
+<template>
+  <i class="material-icons" :class="{'medium': medium, 'circle': circle}"><slot /></i>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    medium: { type: Boolean, default: false, required: false },
+    circle: { type: Boolean, default: false, required: false }
+  }
+});
+</script>
+
+<style>
+
+</style>
