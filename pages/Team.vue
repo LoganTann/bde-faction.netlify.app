b203b299b2f6d20d6d27f15edf0261c2ca7ce56e
LoganTann <logon313@hotmail.fr>
2021-08-21 22:06:31 +0200

    modification du layout de la page Team

--- pages/Team.vue
+++ pages/Team.vue
@@ -36,37 +36,17 @@ export default {
           ]
         },
         {
-          name: "Le pôle Evènementiel",
+          name: "Les pôles",
           members: [
-            { picture: "fabien.jpg", firstName: "Fabien", surName: "Rondan", caption: "Pôle évènementiel" },
-            { picture: ".jpg", firstName: "Fabien", surName: "Wy Suko", caption: "Pôle évènementiel" },
-            { picture: "guillaume.jpg", firstName: "Guillaume", surName: "Merchez", caption: "Pôle évènementiel" }
-          ]
-        },
-        {
-          name: "Le pôle Audiovisuel",
-          members: [
-            { picture: ".jpg", firstName: "Meryem", surName: "Guidas", caption: "Pôle audiovisuel" },
+            { picture: "youcef.jpg", firstName: "Youcef", surName: "Medileh", caption: "Pôle partenariat" },
+            { picture: "none.jpg", firstName: "Meryem", surName: "Guidas", caption: "Pôle audiovisuel" },
             { picture: "nico.jpg", firstName: "Nicolas", surName: "Cuzon", caption: "Pôle audiovisuel" },
-          ]
-        },
-        {
-          name: "Le pôle partenariat",
-          members: [
-            { picture: "youcef.jpg", firstName: "Youcef", surName: "Medileh", caption: "Pôle partenariat" }
-          ]
-        },
-        {
-          name: "Le pôle Communication",
-          members: [
-            { picture: "seb.jpg", firstName: "Sébastien", surName: "Ramirez Meija", caption: "Pôle communication" },
+            { picture: "fabien.jpg", firstName: "Fabien", surName: "Rondan", caption: "Pôle évènementiel" },
+            { picture: "none.jpg", firstName: "Fabien", surName: "WySuko", caption: "Pôle évènementiel" },
+            { picture: "guillaume.jpg", firstName: "Guillaume", surName: "Merchez", caption: "Pôle évènementiel" },
             { picture: "marco.jpg", firstName: "Marco", surName: "Balamon", caption: "Pôle communication" },
-            { picture: "alexis.jpg", firstName: "Alexis", surName: "Charpentreau", caption: "Pôle communication" }
-          ]
-        },
-        {
-          name: "Anciens membres",
-          members: [
+            { picture: "alexis.jpg", firstName: "Alexis", surName: "Charpentreau", caption: "Pôle communication" },
+            { picture: "seb.jpg", firstName: "Sébastien", surName: "Ramirez Meija", caption: "Pôle communication" },
             { picture: "ibra.jpg", firstName: "Ibrahime", surname: "Ahbib", caption: "Ancien Vice-Président" }
           ]
         }
@@ -92,15 +72,11 @@ export default {
       return `${this.pics_path}${picName}`;
     },
     get_equal_column_value (arrayLength, columnName) {
-      let value; // TODO : rendre un poil plus stable
-      switch (arrayLength) {
-        case 2:
-        case 4:
-        case 3:
-          value = 12 / arrayLength;
-          break;
-        default:
-          value = 12;
+      let value = 12;
+      if (arrayLength > 4) {
+        value = 3;
+      } else if (arrayLength > 1) {
+        value = 12 / arrayLength;
       }
       return `${columnName}${value}`
     }
