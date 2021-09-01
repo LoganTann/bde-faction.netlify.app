02b742856b87c12ce29e962c2b483c2f500bf0bd
LoganTann <logon313@hotmail.fr>
2021-09-01 13:06:52 +0200

    modif page Team, modifs ressources.md, ajout article blog rentrée + modif mineure

--- pages/team.vue
+++ pages/team.vue
@@ -4,6 +4,7 @@
       <div v-for="category in member_list" :key="category.name" class="row">
         <div class="col s12 m4">
           <h3>{{ category.name }}</h3>
+          <p>{{ category.desc }}</p>
         </div>
         <div class="col s12 l8 row center">
           <div v-for="member in category.members" :key="member.picture" class="col s12" :class="get_equal_column_value(category.members.length,'m')">
@@ -11,6 +12,17 @@
             <h5>{{ member.firstName }} <span class="surname">{{ member.surName }}</span></h5>
             <p>{{ member.caption }}</p>
           </div>
+        </div>
+      </div>
+
+      <div class="row valign-wrapper">
+        <div class="col s4 l2">
+          <span class="avatar"> <img :src="pic_url('ibra.jpg')" alt="photo de Ibrahime"> </span>
+        </div>
+        <div class="col s8 l10">
+          <h3 style="margin-top: 0;">Entrées et sortie de membres</h3>
+          <p><b>Ibrahime Ahbib</b> (photo ci-contre) était Vice-Président de Faction et a participé à la création du groupe. Il a dû quitter ses fonctions pour des raisons personnelles.</p>
+          <p><b>Fabien Ribeiro</b> était au pôle évènementiel de Sinfonia (la liste bde concurrente à faction) et a accepté de nous rejoindre pour nous assister dans la gestion des évènements.</p>
         </div>
       </div>
     </section>
@@ -27,6 +39,7 @@ export default {
       member_list: [
         {
           name: "Le bureau",
+          desc: "Le président représente le groupe tandis que le vice-président manage l'équipe. Quant aux autres rôles, ceux-ci sont assez explicites.",
           members: [
             { picture: "flo.jpg", firstName: "Florian", surName: "Le Gal", caption: "Président" },
             { picture: "logan.jpg", firstName: "Logan", surName: "Tann", caption: "Vice-Président" },
@@ -36,6 +49,7 @@ export default {
         },
         {
           name: "Le pôle Evènementiel",
+          desc: "Le pôle évènementiel se charge d'inventer et d'organiser les soirées ou sorties.",
           members: [
             { picture: "fabien.jpg", firstName: "Fabien", surName: "Rondan", caption: "Pôle évènementiel" },
             { picture: "none.jpg", firstName: "Fabien", surName: "Ribeiro", caption: "Pôle évènementiel" },
@@ -44,6 +58,7 @@ export default {
         },
         {
           name: "Le pôle Communication",
+          desc: "Manage notre superbe compte Insta, et poste aussi certaines annonces sur Discord.",
           members: [
             { picture: "seb.jpg", firstName: "Sébastien", surName: "Ramirez Meija", caption: "Pôle communication" },
             { picture: "marco.jpg", firstName: "Marco", surName: "Balamon", caption: "Pôle communication" },
@@ -52,6 +67,7 @@ export default {
         },
         {
           name: "Le pôle Audiovisuel",
+          desc: "Crée les superbes infographies pour nos réseaux sociaux.",
           members: [
             { picture: "none.jpg", firstName: "Meryem", surName: "Guldas", caption: "Pôle audiovisuel" },
             { picture: "nico.jpg", firstName: "Nicolas", surName: "Cuzon", caption: "Pôle audiovisuel" }
@@ -59,6 +75,7 @@ export default {
         },
         {
           name: "Le pôle partenariat",
+          desc: "Démarche les entreprises et organisations locales pour trouver des offres exclusives à notre département",
           members: [
             {
               picture: "youcef.jpg",
